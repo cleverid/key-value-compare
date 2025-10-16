e4f927be4462832f4d2565914960284141613512
Eugeny Shaposhnikov <eawapownikov@gmail.com>
2025-10-17 01:48:48 +0300

    plugins. Install

--- picodata-plugins/ttl/src/handlers/endpoints/rpc.rs
+++ picodata-plugins/ttl/src/handlers/endpoints/rpc.rs
@@ -2,7 +2,7 @@ use picodata_plugin::transport::rpc;
 use picodata_plugin::{plugin::prelude::*, transport::rpc::RouteBuilder};
 use serde::{Deserialize, Serialize};
 
-use crate::models::User;
+use crate::models::Profile;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ExampleResponse {
@@ -15,13 +15,13 @@ pub fn register_example_rpc_handle(context: &PicoContext) {
         .register(move |req, _ctx| {
             log::debug!("Received store request: {req:?}");
 
-            let user: User = rmp_serde::from_slice(req.as_bytes()).unwrap();
+            let profile: Profile = rmp_serde::from_slice(req.as_bytes()).unwrap();
 
-            log::warn!("Recieved \"{user:?}\" as RPC input");
+            log::warn!("Recieved \"{profile:?}\" as RPC input");
 
-            let user_name = user.name;
+            let profile_id = profile.id;
             let response_to_return = ExampleResponse {
-                rpc_hello_response: format!("Hello {user_name}, long time no see."),
+                rpc_hello_response: format!("Hello {profile_id}, long time no see."),
             };
 
             Ok(rpc::Response::encode_rmp(&response_to_return).unwrap())
